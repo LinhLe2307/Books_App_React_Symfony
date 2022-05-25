ba53128e0347593aae020268a5c7debbc65e357f
silmu <svetlana.raitina@gmail.com>
2022-05-25 23:18:57 +0300

    Add price to BookCard

--- assets/components/BookCard.js
+++ assets/components/BookCard.js
@@ -1,41 +1,49 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-const BookCard = ({ id, volumeInfo, handleAdd }) => {
-    const bookTitle = volumeInfo.title;
-    const bookImage = volumeInfo.imageLinks?.thumbnail || volumeInfo.imageLinks?.smallThumbnail;
-    const bookAuthors = volumeInfo.authors;
-    const publishedDate = volumeInfo.publishedDate;
+const BookCard = ({ id, volumeInfo, saleInfo, handleAdd }) => {
+  const bookTitle = volumeInfo.title;
+  const bookImage =
+    volumeInfo.imageLinks?.thumbnail || volumeInfo.imageLinks?.smallThumbnail;
+  const bookAuthors = volumeInfo.authors;
+  const publishedDate = volumeInfo.publishedDate;
+  const price = saleInfo.listPrice?.amount;
 
-    // Replacing all the spaces in URL with - for readability. Since the book title is for displaying only, we can use "-", if we use it for search, should use "+" 
-    const titleFormat = bookTitle.replaceAll(" ", "-")
+  // Replacing all the spaces in URL with - for readability. Since the book title is for displaying only, we can use "-", if we use it for search, should use "+"
+  const titleFormat = bookTitle.replaceAll(' ', '-');
 
-    return (
-        <div className='card' style={{ width: "15rem" }}>
+  return (
+    <div className="card" style={{ width: '15rem' }}>
+      {/* This is for taking the image */}
+      <img src={bookImage} alt={bookTitle} className="card-img-top" />
 
-            {/* This is for taking the image */}
-            <img src={bookImage} alt={bookTitle}
-                className="card-img-top" />
+      <div className="card-body">
+        {/* Send the book id and title to BookDetails as :id/:title to display it in URL and retrieve id by useParams().id to fetch data*/}
+        <Link to={`${id}/${titleFormat}`}>
+          <h5 className="card-title">{bookTitle}</h5>
+        </Link>
+        <ul>
+          {/* Link cannot be click when it's Unknown Authors  */}
+          {bookAuthors === 'Unknown Authors' ? (
+            <li>Unknown Authors</li>
+          ) : (
+            bookAuthors?.map((author) => {
+              const authorQuery = author.replaceAll(' ', '+');
+              return (
+                <li key={author}>
+                  <Link to={`/search/author/${authorQuery}`}>{author}</Link>
+                </li>
+              );
+            })
+          )}
+        </ul>
+        <p>Date: {publishedDate}</p>
+        <div>{price ? price + '€' : 'Not for sale'} </div>
+      </div>
 
-            <div className='card-body'>
+      <button onClick={() => handleAdd()}>Add to Cart</button>
+    </div>
+  );
+};
 
-                {/* Send the book id and title to BookDetails as :id/:title to display it in URL and retrieve id by useParams().id to fetch data*/}
-                <Link to={`${id}/${titleFormat}`}><h5 className='card-title'>{bookTitle}</h5></Link>
-                <ul>
-                    {/* Link cannot be click when it's Unknown Authors  */}
-                    {bookAuthors === "Unknown Authors"
-                        ? <li>Unknown Authors</li>
-                        : bookAuthors?.map(author => {
-                            const authorQuery = author.replaceAll(' ', '+');
-                            return <li key={author}><Link to={`/search/author/${authorQuery}`}>{author}</Link></li>
-                        })}
-                </ul>
-                <p>Date: {publishedDate}</p>
-            </div>
-
-            <button onClick={() => handleAdd()}>Add to Cart</button>
-        </div>
-    )
-}
-
-export default BookCard
+export default BookCard;
--- assets/components/SearchPage.js
+++ assets/components/SearchPage.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router";
-import axios from "axios";
-import BookCard from "./BookCard";
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import axios from 'axios';
+import BookCard from './BookCard';
 
 const SearchPage = () => {
   const keyword = useParams().keyword; //Since users' input matches the keyword in URL, we can use it after q= to get the data
   const authorName = useParams().name; //Since author's name matches the keyword in URL, we can use it after q= to get the data
   const [books, setBooks] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [sort, setSort] = useState("");
+  const [sort, setSort] = useState('');
 
   // fetch books from Google API based on users' inputs. Default volume is 10 but we can get the maximum allowable results up to 40
   const fetchBooks = () => {
@@ -50,21 +50,21 @@ const SearchPage = () => {
   const handleAvailableData = (res) => {
     // If there are no date, images or title, make default value
     const availableData = res.data.items.map((book) => {
-      if (book.volumeInfo.hasOwnProperty("publishedDate") === false) {
-        book.volumeInfo.publishedDate = "0000";
+      if (book.volumeInfo.hasOwnProperty('publishedDate') === false) {
+        book.volumeInfo.publishedDate = '0000';
       }
-      if (book.volumeInfo.hasOwnProperty("imageLinks") === false) {
+      if (book.volumeInfo.hasOwnProperty('imageLinks') === false) {
         book.volumeInfo.imageLinks = {
           thumbnail:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRd-y-IJN8glQlf1qoU01dEgGPUa0d1-sjfWg&usqp=CAU",
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRd-y-IJN8glQlf1qoU01dEgGPUa0d1-sjfWg&usqp=CAU',
         };
       }
-      if (book.volumeInfo.hasOwnProperty("title") === false) {
-        book.volumeInfo.title = "No Title";
+      if (book.volumeInfo.hasOwnProperty('title') === false) {
+        book.volumeInfo.title = 'No Title';
       }
 
-      if (book.volumeInfo.hasOwnProperty("authors") === false) {
-        book.volumeInfo.authors = "Unknown Authors";
+      if (book.volumeInfo.hasOwnProperty('authors') === false) {
+        book.volumeInfo.authors = 'Unknown Authors';
       }
       return book;
     });
@@ -74,10 +74,10 @@ const SearchPage = () => {
   // This is for comparing years, months and dates between books
   const sortedDate = books.sort((a, b) => {
     // split the publishedDate to year, month, date and sort based on year, then month, then date
-    const [aYear, aMonth, aDate] = a.volumeInfo.publishedDate.split("-");
-    const [bYear, bMonth, bDate] = b.volumeInfo.publishedDate.split("-");
+    const [aYear, aMonth, aDate] = a.volumeInfo.publishedDate.split('-');
+    const [bYear, bMonth, bDate] = b.volumeInfo.publishedDate.split('-');
 
-    if (sort === "newest") {
+    if (sort === 'newest') {
       if (parseInt(aYear) === parseInt(bYear)) {
         if (parseInt(aMonth) === parseInt(bMonth)) {
           if (parseInt(aDate) < parseInt(bDate)) return -1;
@@ -87,8 +87,7 @@ const SearchPage = () => {
         else return 1;
       } else if (parseInt(aYear) > parseInt(bYear)) return -1;
       else return 1;
-      
-    } else if (sort === "oldest") {
+    } else if (sort === 'oldest') {
       if (parseInt(bYear) === parseInt(aYear)) {
         if (parseInt(bMonth) === parseInt(aMonth)) {
           if (parseInt(bDate) < parseInt(aDate)) return -1;
@@ -108,11 +107,11 @@ const SearchPage = () => {
 
   useEffect(() => {
     // keyword will be undefined whenever the url changes to search the authorName. It will still render even undefined everytime the author's name is click => double rendering
-    keyword ? fetchBooks() : "";
+    keyword ? fetchBooks() : '';
   }, [keyword]);
 
   useEffect(() => {
-    authorName ? handleAuthor() : "";
+    authorName ? handleAuthor() : '';
   }, [authorName]);
 
   if (isLoading) {
