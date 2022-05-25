94afe1bce0d210afcffc661f8dc84864dbcaf635
silmu <svetlana.raitina@gmail.com>
2022-05-25 22:51:41 +0300

    Add GET and POST for fetching orders from SQL db

--- assets/components/Checkout.js
+++ assets/components/Checkout.js
@@ -1,6 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Swal from 'sweetalert2';
+import axios from 'axios';
 
 const Checkout = (props) => {
+  const [orders, setOrders] = useState([]);
+  const [address, setAddress] = useState({
+    firstname: '',
+    lastname: '',
+    address: '',
+    productName: '',
+  });
+  const [isSaving, setIsSaving] = useState(false);
+
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
+  const fetchOrders = () => {
+    axios
+      .get('/api/checkout')
+      .then((res) => {
+        setOrders(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log('Axios error: ', err);
+      });
+  };
+
+  const handlePost = () => {
+    setIsSaving(true);
+    let formData = new FormData();
+    let dataToSend =
+      address.productName +
+      ', ' +
+      address.firstname +
+      ' ' +
+      address.lastname +
+      ', ' +
+      address.address;
+    formData.append('address', dataToSend);
+    axios
+      .post('/api/checkout', formData)
+      .then((res) => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Order placed successfully',
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        setIsSaving(false);
+        setAddress('');
+      })
+      .catch((err) => {
+        console.log('Axios error: ', err);
+        Swal.fire({
+          icon: 'error',
+          title: 'An error occured',
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        setIsSaving(false);
+      });
+  };
+
   return (
     <div>
       {/*....... Billing details .......*/}
@@ -8,17 +71,48 @@ const Checkout = (props) => {
       <form className="border rounded m-3 p-3">
         <div className="form-group ">
           <label htmlFor="firstname">First name</label>
-          <input type="text" className="form-control" name="firstname"></input>
+          <input
+            type="text"
+            className="form-control"
+            name="firstname"
+            onChange={(e) => {
+              setAddress({ ...address, firstname: e.target.value });
+            }}
+          ></input>
         </div>
         <div className="form-group">
           <label htmlFor="lastname">Last name</label>
-          <input type="text" className="form-control" name="lastname"></input>
+          <input
+            type="text"
+            className="form-control"
+            name="lastname"
+            onChange={(e) => {
+              setAddress({ ...address, lastname: e.target.value });
+            }}
+          ></input>
         </div>
         <div className="form-group">
           <label htmlFor="address">Address</label>
-          <input type="text" className="form-control" name="address"></input>
+          <input
+            type="text"
+            className="form-control"
+            name="address"
+            onChange={(e) => {
+              setAddress({ ...address, address: e.target.value });
+            }}
+          ></input>
         </div>
-        <button className="btn btn-primary">SUBMIT</button>
+        <div className="form-group">
+          <label htmlFor="address">Book name</label>
+          <input
+            type="text"
+            className="form-control"
+            name="productName"
+            onChange={(e) => {
+              setAddress({ ...address, productName: e.target.value });
+            }}
+          ></input>
+        </div>
       </form>
       {/*....... Order overview .......*/}
       <div className="border rounded m-3 p-3">
@@ -26,22 +120,50 @@ const Checkout = (props) => {
         <table className="table">
           <thead>
             <tr>
-              <th scope="col">Product</th>
-              <th scope="col">Price</th>
+              <th scope="col">Book name</th>
+              <th scope="col">First name</th>
+              <th scope="col">Last name</th>
+              <th scope="col">Address</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>Item 1</td>
-              <td>20€</td>
-            </tr>
-            <tr>
-              <td>Item 2</td>
-              <td>18€</td>
+              <td>{address.productName}</td>
+              <td>{address.firstname}</td>
+              <td>{address.lastname}</td>
+              <td>{address.address}</td>
             </tr>
           </tbody>
         </table>
-        <button className="btn btn-primary">PLACE ORDER</button>
+        <button
+          disabled={isSaving}
+          className="btn btn-primary"
+          onClick={handlePost}
+        >
+          PLACE ORDER
+        </button>
+      </div>
+      {/*....... Order history .......*/}
+      <div className="border rounded m-3 p-3">
+        <h4>Order history</h4>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Order ID</th>
+              <th scope="col">Order info</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order, key) => {
+              return (
+                <tr key={key}>
+                  <td>{order.order_id}</td>
+                  <td>{order.address}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     </div>
   );
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Orders;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,6 +18,33 @@ class MainController extends AbstractController
     {
         return $this->render('main/index.html.twig');
         
+    }
+
+    #[Route('/api/checkout', name: 'checkout', methods:['GET'])]
+    public function listOrders(EntityManagerInterface $em): Response
+    {
+        $orders = $em->getRepository(Orders::class)->findAll();
+        $data = [];
+        foreach($orders as $order){
+            $data[] = [
+                'order_id' => $order->getOrderId(),
+                'user_id' => $order->getUserId(),
+                'address' => $order->getAddress(),
+            ];
+        }
+        return $this->json($data);
+    }
+
+    #[Route('/api/checkout', name: 'place_order', methods:['POST'])]
+    public function placeOrder(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $em = $doctrine->getManager();
+        $order = new Orders();
+        $order->setAddress($request->request->get('address'));
+        $em->persist($order);
+        $em->flush();
+
+        return $this->json('Placed order successfully');
     }
     
 }
