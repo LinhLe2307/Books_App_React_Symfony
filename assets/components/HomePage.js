c6118d9cc143b29410529e0d90dfb877338eef70
silmu <svetlana.raitina@gmail.com>
2022-05-26 14:38:03 +0300

    Add fetch all classic books

--- assets/components/HomePage.js
+++ assets/components/HomePage.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import BookCard from './BookCard';
 
 const HomePage = () => {
-  return <div>Hello Homepage</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const fetchBooks = () => {
+    setIsLoading(true);
+    axios
+      .get(
+        `https://www.googleapis.com/books/v1/volumes?q=classics&filter=paid-ebooks&maxResults=40`
+      )
+      .then((res) => {
+        console.log(res.data.items);
+        setBooks(res.data.items);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <div className="justify-content-center d-flex p-2 flex-wrap">
+        {books.map((book, key) => {
+          return <BookCard key={key} {...book} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
