0a3f1b97be3c1b524ea7ff63e78863290d531b4f
Sveta <60633351+silmu@users.noreply.github.com>
2022-10-17 23:12:56 +0300

    Add Embedded Viewer API for previewing content of a book

--- assets/components/BookDetails.js
+++ assets/components/BookDetails.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { Link, useParams, useLocation } from "react-router-dom";
-import parse from "html-react-parser";
-import axios from "axios";
-import { handleIndividualData } from "../handleIndividualData";
+import React, { useEffect, useState } from 'react';
+import { Link, useParams, useLocation } from 'react-router-dom';
+import parse from 'html-react-parser';
+
+import BookViewer from './BookViewer';
 
 const BookDetails = ({ click }) => {
+  const [showPreview, setShowPreview] = useState(true);
   const location = useLocation();
   const bookInfo = location.state.data;
   const { volumeInfo, saleInfo } = bookInfo;
@@ -17,85 +18,98 @@ const BookDetails = ({ click }) => {
   const description = volumeInfo?.description;
   const price = saleInfo?.listPrice?.amount;
   const currency = saleInfo?.listPrice?.currencyCode;
-  const language = volumeInfo?.language ?? "";
+  const language = volumeInfo?.language ?? '';
   const categories = volumeInfo?.categories ?? [];
   const ISBN = volumeInfo?.industryIdentifiers ?? [];
-  const pages = volumeInfo?.pageCount ?? "";
-  const publisher = volumeInfo?.publisher ?? "";
-  let mature = volumeInfo?.maturityRating ?? "";
-  mature = mature == "MATURE" ? true : false;
+  const pages = volumeInfo?.pageCount ?? '';
+  const publisher = volumeInfo?.publisher ?? '';
+  let mature = volumeInfo?.maturityRating ?? '';
+  mature = mature == 'MATURE' ? true : false;
+
+  const handlePreview = () => {
+    console.log(showPreview);
+    setShowPreview(!showPreview);
+  };
 
   return (
-    <div className="container book-details mb-3">
-    <div className="row g-5">
-      {/*............. Left column ..........*/}
-      <div className="col">
-        {/*............. Book image ..........*/}
-        <div className="image-container">
-          <img
-            className="book-details-image mt-4 mb-5"
-            src={bookImage}
-            alt={bookTitle}
-          />
-          {/*........ Mature content warning ......*/}
-          <div className="mature">{mature ? "🔞" : ""}</div>
-        </div>
-        {/*........ Language, categories, pages........*/}
-        <h4 className="d-inline">Language:</h4>
-        <span>{" " + language}</span>
-        <h4>Categories:</h4>
-        <div className="categories">
-          {categories.map((category, key) => {
-            return <div key={key}>{category}</div>;
-          })}
-        </div>
-        <h4 className="d-inline">Pages:</h4>
-        <span>{" " + pages}</span>
-      </div>
-      {/*............. Right column ..........*/}
-      <div className="col-9">
-        <h1>{bookTitle}</h1>
-        <div>
-          {/*............. Authors ..........*/}
-          <h2>Author:</h2>
-          {/* Link cannot be click when it's Unknown Authors  */}
-          {bookAuthors === "Unknown Authors"
-            ? "Unknown Authors"
-            : bookAuthors?.map((author) => {
-                const authorQuery = author.replaceAll(" ", "+");
-                return (
-                  <h3 key={author}>
-                    <Link to={`/search/author/${authorQuery}`}>
-                      {author + " "}
-                    </Link>
-                  </h3>
-                );
-              })}
-        </div>
-        {/*............. Date, publisher, ISBN ..........*/}
-        <div className="book-date text-muted">Date: {publishedDate}</div>
-        <div className="text-muted">Publisher: {publisher}</div>
-        {ISBN.map((id, key) => {
-          return (
-            <div className="isbn" key={key}>
-              {id.type}: {id.identifier}
+    <div>
+      <div className='container book-details mb-3'>
+        <div className='row g-5'>
+          {/*............. Left column ..........*/}
+          <div className='col'>
+            {/*............. Book image ..........*/}
+            <div className='image-container'>
+              <img
+                className='book-details-image mt-4 mb-5'
+                src={bookImage}
+                alt={bookTitle}
+              />
+              {/*........ Mature content warning ......*/}
+              <div className='mature'>{mature ? '🔞' : ''}</div>
             </div>
-          );
-        })}
-        {/*............. Description ..........*/}
-        {/* Parse string to HTML */}
-        <div className="book-description mt-3">{parse(`${description}`)}</div>
-        <div className="book-price mt-3">
-          {price} {currency}
+            {/*........ Language, categories, pages........*/}
+            <h4 className='d-inline'>Language:</h4>
+            <span>{' ' + language}</span>
+            <h4>Categories:</h4>
+            <div className='categories'>
+              {categories.map((category, key) => {
+                return <div key={key}>{category}</div>;
+              })}
+            </div>
+            <h4 className='d-inline'>Pages:</h4>
+            <span>{' ' + pages}</span>
+          </div>
+          {/*............. Right column ..........*/}
+          <div className='col-9'>
+            <h1>{bookTitle}</h1>
+            <div>
+              {/*............. Authors ..........*/}
+              <h2>Author:</h2>
+              {/* Link cannot be click when it's Unknown Authors  */}
+              {bookAuthors === 'Unknown Authors'
+                ? 'Unknown Authors'
+                : bookAuthors?.map(author => {
+                    const authorQuery = author.replaceAll(' ', '+');
+                    return (
+                      <h3 key={author}>
+                        <Link to={`/search/author/${authorQuery}`}>
+                          {author + ' '}
+                        </Link>
+                      </h3>
+                    );
+                  })}
+            </div>
+            {/*............. Date, publisher, ISBN ..........*/}
+            <div className='book-date text-muted'>Date: {publishedDate}</div>
+            <div className='text-muted'>Publisher: {publisher}</div>
+            {ISBN.map((id, key) => {
+              return (
+                <div className='isbn' key={key}>
+                  {id.type}: {id.identifier}
+                </div>
+              );
+            })}
+            {/*............. Description ..........*/}
+            {/* Parse string to HTML */}
+            <div className='book-description mt-3'>
+              {parse(`${description}`)}
+            </div>
+            <div className='book-price mt-3'>
+              {price} {currency}
+            </div>
+            {/*............... Button ..............*/}
+            <button className='btn btn-primary' onClick={() => click(bookInfo)}>
+              Add to Cart
+            </button>
+          </div>
         </div>
-        {/*............... Button ..............*/}
-        <button className="btn btn-primary" onClick={() => click(bookInfo)}>
-          Add to Cart
+        <button className='btn btn-primary' onClick={handlePreview}>
+          {showPreview ? 'Hide preview' : 'Show preview'}
         </button>
+        {showPreview ? <BookViewer ISBN_num={ISBN[0].identifier} /> : ''}
       </div>
     </div>
-  </div>
-);
+  );
 };
 
 export default BookDetails;
